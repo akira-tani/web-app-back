9a0962836bd887218b7d63ecffdffc468363e756
akira-taniguchi <fesr82d5@yahoo.ne.jp>
2022-04-09 22:15:31 +0900

    second

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 Route::apiResource('/user', UserController::class)->only([
     'index', 'store', 'show'
+]);
+
+Route::apiResource('/post', UserController::class)->only([
+    'index', 'store', 'show', 'destroy'
 ]);
